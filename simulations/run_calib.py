e4854a9daec55056e222384d54eb8b04fdbfb6b4
tobias-holden <52420991+tobias-holden@users.noreply.github.com>
2024-10-07 19:22:08 -0500

    Update run_calib.py

--- simulations/run_calib.py
+++ simulations/run_calib.py
@@ -11,7 +11,7 @@ from emulators.GP import ExactGP
 from bo import BO 
 from plot import * 
 from my_func import my_func as myFunc 
-from compare_to_data.run_full_comparison import plot_prevalence,plot_incidence,compute_scores_across_site,save_EIR,save_AnnualIncidence 
+from compare_to_data.run_full_comparison import plot_prevalence,plot_incidence,compute_scores_across_site,save_maxEIR,save_AnnualIncidence 
 from clean_all import clean_analyzers 
 from translate_parameters import translate_parameters
 
@@ -19,7 +19,7 @@ import manifest as manifest
 
 # Experiment details
 Site="Nanoro"
-exp_label = "Nanoro_test_flat2"
+exp_label = "241007_test2"
 output_dir = f"output/{exp_label}"
 best_dir = f"output/{exp_label}" 
 
@@ -28,17 +28,17 @@ calib_coord = pd.read_csv(os.path.join(manifest.input_files_path,"calibration_co
 # Botorch details
 calib_coord = calib_coord.set_index("site")
 init_samples=int(calib_coord.at[Site,"init_size"])
-init_batches =  1
+init_batches =  int(calib_coord.at[Site,"init_batches"]) #1 
 emulator_batch_size = int(calib_coord.at[Site, 'batch_size'])
 gp_max_eval = int(calib_coord.at[Site, 'max_eval'])
 failure_limit = int(calib_coord.at[Site, 'failure_limit'])
 
-
+param_key=pd.read_csv("test_parameter_key.csv")
 
 # Define the Problem, it must be a functor
 class Problem:
     def __init__(self,workdir="checkpoints/emod"):
-        self.dim = 4 # mandatory dimension
+        self.dim = int(param_key.shape[0])  #4 # mandatory dimension
         self.ymax = None #max value
         self.best = None
         self.n = 0
@@ -55,7 +55,7 @@ class Problem:
     # The input is a vector that contains multiple set of parameters to be evaluated
     def __call__(self, X):
         
-        param_key=pd.read_csv("test_parameter_key.csv")
+        
         wdir=os.path.join(f"{self.workdir}/LF_{self.n}")
         os.makedirs(wdir,exist_ok=True)
             
@@ -102,12 +102,12 @@ class Problem:
             self.best.to_csv(f"{self.workdir}/LF_{self.n}/emod.best.csv")
             Y0['round'] = [self.n] * len(Y0)
             Y0.to_csv(f"{self.workdir}/all_scores.csv")
-            mEIR = save_EIR(site=Site, wdir = f"{self.workdir}/LF_{self.n}")
+            mEIR = save_maxEIR(site=Site, wdir = f"{self.workdir}/LF_{self.n}")
             mEIR.to_csv(f"{self.workdir}/LF_{self.n}/EIR_range.csv")
             ACI = save_AnnualIncidence(site=Site, wdir =f"{self.workdir}/LF_{self.n}")
             ACI.to_csv(f"{self.workdir}/LF_{self.n}/ACI.csv")
-            plot_prevalence(round_num=self.n,site=Site, plt_dir=os.path.join(f"{self.workdir}/LF_{self.n}"), wdir=os.path.join(f"{self.workdir}/LF_{self.n}"))
-            plot_incidence(round_num=self.n,site=Site, plt_dir=os.path.join(f"{self.workdir}/LF_{self.n}"), wdir=os.path.join(f"{self.workdir}/LF_{self.n}"))
+            plot_prevalence(site=Site, plt_dir=os.path.join(f"{self.workdir}/LF_{self.n}"), wdir=os.path.join(f"{self.workdir}/LF_{self.n}"))
+            plot_incidence(site=Site, plt_dir=os.path.join(f"{self.workdir}/LF_{self.n}"), wdir=os.path.join(f"{self.workdir}/LF_{self.n}"))
             shutil.copytree(f"{manifest.simulation_output_filepath}",f"{self.workdir}/LF_{self.n}/SO")
             self.n += 1
             np.savetxt(f"{self.workdir}/emod.n.txt", [self.n])
@@ -121,12 +121,12 @@ class Problem:
                 best_p = max(pset,key=pset.get)
                 self.best = translate_parameters(param_key,best_x,best_p)
                 self.best.to_csv(f"{self.workdir}/LF_{self.n}/emod.best.csv")
-                mEIR = save_EIR(site=Site, wdir = f"{self.workdir}/LF_{self.n}")
+                mEIR = save_maxEIR(site=Site, wdir = f"{self.workdir}/LF_{self.n}")
                 mEIR.to_csv(f"{self.workdir}/LF_{self.n}/EIR_range.csv")
                 ACI = save_AnnualIncidence(site=Site, wdir =f"{self.workdir}/LF_{self.n}")
                 ACI.to_csv(f"{self.workdir}/LF_{self.n}/ACI.csv")
-                plot_prevalence(round_num=self.n,site=Site, plt_dir=os.path.join(f"{self.workdir}/LF_{self.n}"), wdir=os.path.join(f"{self.workdir}/LF_{self.n}"))
-                plot_incidence(round_num=self.n,site=Site, plt_dir=os.path.join(f"{self.workdir}/LF_{self.n}"), wdir=os.path.join(f"{self.workdir}/LF_{self.n}"))
+                plot_prevalence(site=Site, plt_dir=os.path.join(f"{self.workdir}/LF_{self.n}"), wdir=os.path.join(f"{self.workdir}/LF_{self.n}"))
+                plot_incidence(site=Site, plt_dir=os.path.join(f"{self.workdir}/LF_{self.n}"), wdir=os.path.join(f"{self.workdir}/LF_{self.n}"))
                 np.savetxt(f"{self.workdir}/emod.ymax.txt", [self.ymax])
                 np.savetxt(f"{self.workdir}/LF_{self.n}/emod.ymax.txt", [self.ymax])
             Y0['round'] = [self.n] * len(Y0)
@@ -174,8 +174,8 @@ bo.initRandom(init_samples, n_batches = init_batches)
 # Run the optimization loop
 bo.run(exp_label=f"{exp_label}")
 
-x=pd.read_csv("test_parameter_key.csv")
-parameter_labels=x['parameter_label'].to_list()
+x=param_key
+parameter_labels=param_key['parameter_label'].to_list()
 
 # Plot
 plot_runtimes(bo),
@@ -186,11 +186,9 @@ plot_convergence(bo, negate=True, ymin=-100, ymax=0)
 plt.savefig(f'output/{exp_label}/convergence', bbox_inches="tight")
 plot_prediction_error(bo)
 plt.savefig(f'output/{exp_label}/pred_error', bbox_inches="tight")
-plot_X_flat(bo, param_key = x, labels=parameter_labels)
+plot_X_flat(bo, param_key = param_key, labels=parameter_labels)
 plt.savefig(f'output/{exp_label}/x_flat', bbox_inches="tight")
 ##plot_space(bo, -5**2, 0, labels="X")
 ##plt.savefig('output/{exp_label}/space', bbox_inches="tight")
 ##plot_y_vs_posterior_mean(bo,n_init=1)
 ##plt.savefig('output/{exp_label}/posterior_mean', bbox_inches="tight")
-
-
