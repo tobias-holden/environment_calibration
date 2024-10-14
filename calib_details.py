9fe1f5cf40d06795f439f5f4ee20d0a462d1067b
tobias-holden <52420991+tobias-holden@users.noreply.github.com>
2024-10-13 20:26:41 -0500

    Update calib_details.py
    wrap post-analysis plotters in functions

--- calib_details.py
+++ calib_details.py
@@ -4,15 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-experiment = '241007_test2'
-
-plot_predictions = True
-exclude_count = 0
-plot_length_scales = True
-plot_timers = True
-
-print(" ".join(("Loading botorch objects for experiment",experiment)))
-
 #print("iterations")
 #iterations = torch.load(os.path.join('simulations','output',experiment,'iterations.pt'))
 #print(iterations.shape)
@@ -21,8 +12,7 @@ print(" ".join(("Loading botorch objects for experiment",experiment)))
 #X = torch.load(os.path.join('simulations','output',experiment,'X.pt'))
 #print(X.shape)
 
-if plot_predictions:
-    print("Y")
+def plot_calibration_predictions(experiment='',exclude_count=0):
     Y = torch.load(os.path.join('simulations','output',experiment,'Y.pt'))
     print(Y.shape)
 
@@ -41,27 +31,20 @@ if plot_predictions:
     n_missing=len(mean)-len(Y)
     if n_missing>0:
         Y=np.append(Y,np.repeat(0.1,n_missing))
-        #print(mean)
-
-
+        
     if len(mean) > exclude_count:
         Y = Y[exclude_count:]
         mean = mean[exclude_count:]
         var = var[exclude_count:]
-
         # Calculate standard deviation
         std_dev = np.sqrt(var)
-
         # Define confidence interval (e.g., 95% confidence interval)
         confidence_interval = 1.96 * std_dev
-
         # Define x values (e.g., indices or other x-axis values)
         x = np.arange(len(mean))
-
         # Plot the mean predictions
         plt.figure(figsize=(10, 6))
         plt.plot(x, mean, label='Predicted Mean', color='blue')
-
         # Plot the confidence interval
         plt.fill_between(x, mean - confidence_interval, mean + confidence_interval, color='blue', alpha=0.2, label='95% Confidence Interval')
         plt.plot(x,Y,label='Observation',marker='o',color='blue',linestyle='')
@@ -70,26 +53,22 @@ if plot_predictions:
         plt.ylabel('Predicted Value')
         plt.title('Predictions with Confidence Interval')
         plt.legend()
-
         # Show the plot
         plt.show()
+        # Save the plot
         plt.savefig(os.path.join("simulations","output",experiment,'_'.join((experiment,'predictions.png'))), dpi=300)  # Save as PNG with high resolution
         plt.savefig(os.path.join("simulations","output",experiment,'_'.join((experiment,'predictions.pdf'))))  
-
     else:
         print("The tensor length is less than or equal to the number of values to exclude.")
 
 
-if plot_length_scales:
-    print("length_scales")
+def plot_calibration_lengthscales(experiment=''):
     length_scales = torch.load(os.path.join('simulations','output',experiment,'length_scales.pt'))
-    print(length_scales.shape)
     length_scales = torch.transpose(length_scales,0,1)
     nparam=list(length_scales.shape)[0]
     rounds=list(length_scales.shape)[1]
     parameter_names = pd.read_csv(os.path.join('simulations','test_parameter_key.csv'))
     parameter_names = parameter_names['parameter_label'].tolist()
-    #print(parameter_names)
     # plotting
     num_plots = nparam
     cols = int(np.ceil(np.sqrt(num_plots)))  # Number of columns in the grid
@@ -108,28 +87,20 @@ if plot_length_scales:
         axes[j].axis('off')
     # Adjust layout
     plt.tight_layout()
+    # Show plot
     plt.show()
     # Save the plot as PNG and PDF
     fig.savefig(os.path.join("simulations","output",experiment,'_'.join((experiment,'lengthscales.png'))), dpi=300)  # Save as PNG with high resolution
     fig.savefig(os.path.join("simulations","output",experiment,'_'.join((experiment,'lengthscales.pdf'))))  
 
 
-#### TIMING 
-if plot_timers:
-    print("timer_model")
+def plot_calibration_timing(experiment=''):
     timer_model = torch.load(os.path.join('simulations','output',experiment,'timer_model.pt'))
-    print(timer_model.shape)
-
-    print("timer_batch_generator")
     timer_batch_generator = torch.load(os.path.join('simulations','output',experiment,'timer_batch_generator.pt'))
-    print(timer_batch_generator.shape)
-
     timer_model = timer_model.numpy().flatten()
     timer_batch_generator = timer_batch_generator.numpy().flatten()
-
     # Define x values (e.g., indices or other x-axis values)
     x = np.arange(len(timer_model))
-
     # Plot the mean predictions
     plt.figure(figsize=(10, 6))
     plt.plot(x, timer_model, label='Model Fitting', color='blue')
@@ -139,8 +110,8 @@ if plot_timers:
     plt.ylabel('Time (sec)')
     plt.title('Timing')
     plt.legend()
-
     # Show the plot
     plt.show()
+    # Save plot
     plt.savefig(os.path.join("simulations","output",experiment,'_'.join((experiment,'timing.png'))), dpi=300)  # Save as PNG with high resolution
-    plt.savefig(os.path.join("simulations","output",experiment,'_'.join((experiment,'timing.pdf'))))  
+    plt.savefig(os.path.join("simulations","output",experiment,'_'.join((experiment,'timing.pdf'))))
