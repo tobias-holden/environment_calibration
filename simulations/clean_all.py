b5f986e6add880584c1d25b828614c49229b8ac7
tobias-holden <52420991+tobias-holden@users.noreply.github.com>
2024-10-13 20:39:59 -0500

    Update clean_all.py
    added clean_logs() function to delete accumulated .err and .out files inside /logs

--- simulations/clean_all.py
+++ simulations/clean_all.py
@@ -8,5 +8,11 @@ def clean_analyzers():
         if re.match("run_analyzer_.*sh", file) or re.match("wait_analyzer_.*sh", file):
             os.remove(os.path.join(manifest.CURRENT_DIR,file))
 
+def clean_logs():
+    log_dir= manifest.CURRENT_DIR / "logs"
+    for file in os.listdir(log_dir):
+            os.remove(os.path.join(log_dir,file))
+
 if __name__ == "__main__":
     clean_analyzers()
+    clean_logs()
