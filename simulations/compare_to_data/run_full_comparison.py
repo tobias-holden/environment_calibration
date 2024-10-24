9c2e8690292ea735f39bc540659821131d6bcff1
tobias-holden <tobias.holden@northwestern.ed>
2024-10-23 23:29:57 -0500

    update fit-related plots and post-calibration plots

--- simulations/compare_to_data/run_full_comparison.py
+++ simulations/compare_to_data/run_full_comparison.py
@@ -59,7 +59,6 @@ def plot_incidence(site="",plt_dir=os.path.join(manifest.simulation_output_filep
     case_df = case_df[case_df['age']==agebin]
     # convert case_df 'year' to start at 0, like simulations
     case_df['year'] = [y - int(start_year) for y in case_df['year']]
-    print(case_df)
     # sum incidence across year    
     case_df=case_df.merge(case_df.groupby('year')['case'].agg(np.nanmax).reset_index(name='max_incd'), on='year',how='left')
     # normalize monthly incidence so each year sums to 1
@@ -74,19 +73,19 @@ def plot_incidence(site="",plt_dir=os.path.join(manifest.simulation_output_filep
     sim_cases['Inc'] = sim_cases['Cases'] / sim_cases['Pop']
     sim_cases=sim_cases.merge(sim_cases.groupby(['Sample_ID','Year'])['Inc'].agg(np.nanmax).reset_index(name='max_simincd'), on=['Sample_ID'],how='left').reset_index()
     sim_cases['norm_simincd'] = sim_cases.apply(lambda row: (row['Inc'] / row['max_simincd']), axis=1)
-    #print(sim_cases)
     # get mean normalized incidence by month/sample_id (across years)
     sim_cases = sim_cases.groupby(['Sample_ID', 'month'])['norm_simincd'].agg(np.nanmean).reset_index()
     # merge simulated normalized monthly incidence with reference data on ['month']
     case_df = sim_cases.merge(rcases, on ='month')
     case_df = case_df.dropna(subset=['norm_simincd']).reset_index()
     case_df = case_df[case_df['Sample_ID']==best]
+    
     #print(rcases1)
     #print(sim_cases1)
     plt.clf()
-    plt.figure(figsize=(6, 6), dpi=300, tight_layout=True)
-    plt.plot(case_df['month'],case_df['norm_simincd'],label="Simulation")
+    plt.figure(figsize=(6, 6), dpi=300, tight_layout=True)        
     plt.scatter(case_df['month'], case_df['norm_repincd'], label="Reference",color='k')
+    plt.plot(case_df['month'],case_df['norm_simincd'],label="Simulation")
     plt.legend()
     plt.xlabel("Month")
     plt.xticks(np.arange(1, 13))
@@ -157,6 +156,7 @@ def plot_allAge_prevalence(site="",plt_dir=os.path.join(manifest.simulation_outp
     
     refpcr = pd.read_csv(os.path.join(manifest.base_reference_filepath,
                                       coord_df.at['prevalence_comparison_reference','value']))
+    refpcr['date']=np.nan
     # ref_date_format = '%m/%d/%Y'
     for index, row in refpcr.iterrows():
         m=refpcr['month'][index]
@@ -168,8 +168,8 @@ def plot_allAge_prevalence(site="",plt_dir=os.path.join(manifest.simulation_outp
         # refpcr['year'][index] = dayof.year
     plt.clf()
     plt.figure(figsize=(6, 6), dpi=300, tight_layout=True)
-    plt.scatter(refpcr['date'], refpcr['ref_prevalence'], label="Reference", color='k')
     plt.plot(sim_df['date'],sim_df['PCR Parasite Prevalence'], label="Simulation")
+    plt.scatter(refpcr['date'], refpcr['ref_prevalence'], label="Reference", color='k')
     plt.legend()
     plt.xlabel("Date")
     plt.ylabel("PCR Parasite Prevalence")
@@ -180,7 +180,7 @@ def plot_allAge_prevalence(site="",plt_dir=os.path.join(manifest.simulation_outp
 
 if __name__ == "__main__":
 
-    workdir="/projects/b1139/environment_calibration/simulations/output/test_repo2/"
+    workdir="/projects/b1139/environment_calibration/simulations/output/test_repo4/"
     site="Nanoro"
     n=0
     #print(compute_scores_across_site(site))
@@ -188,6 +188,6 @@ if __name__ == "__main__":
     #EIR.to_csv(f"{workdir}/LF_{n}/EIR_range.csv")
     #ACI = save_AnnualIncidence(site=site, wdir =f"{workdir}/LF_{n}",agebin=100)
     #ACI.to_csv(f"{workdir}/LF_{n}/ACI.csv")
-    ##plot_incidence(site=site, plt_dir=os.path.join(f"{workdir}/LF_{n}"), wdir=os.path.join(f"{workdir}/LF_{n}"),agebin=100)
+    plot_incidence(site=site, plt_dir=os.path.join(f"{workdir}/LF_{n}"), wdir=os.path.join(f"{workdir}/LF_{n}"),agebin=100)
     plot_allAge_prevalence(site=site, plt_dir=os.path.join(f"{workdir}/LF_{n}"), wdir=os.path.join(f"{workdir}/LF_{n}"))
 
