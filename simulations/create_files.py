2c26fb0afc5551eeff9d59522ec0e062382b7171
tobias-holden <tobias.holden@northwestern.ed>
2024-10-25 19:55:47 -0500

    shared files moved to environment_calibration_common submodule

--- simulations/create_files.py
+++
@@ -1,7 +0,0 @@
-from helpers import generate_demographics, extract_climate
-
-if __name__ == '__main__':
-    print("CREATING DEMOGRAPHICS")
-    generate_demographics()
-    print("REQUESTING CLIMATE - You may need to log in")
-    extract_climate(flatten_temp=True)
